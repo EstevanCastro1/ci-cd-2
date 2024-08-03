8322132764f0b1c35e38027a179817eefc82d8e6
ecastro5 <ecastro@skgtecnologia.com>
2024-08-02 23:28:38 -0500

    uo

--- devops_prueba/app/test.py
+++ devops_prueba/app/test.py
@@ -7,11 +7,11 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def home():
-    greeting = os.getenv('GREETING', '¡Hola, devc!')
+    greeting = os.getenv('GREETING', '¡Hola, dev!')
     return jsonify({"message": greeting})
 
 def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert response.get_json() == {"message": "¡Hola, devco!"}
+    assert response.get_json() == {"message": "¡Hola, dev!"}
