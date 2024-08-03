556f1206382a1ad482da66948b4d4a683fc2cfee
ecastro5 <ecastro@skgtecnologia.com>
2024-08-02 23:58:49 -0500

    up

--- devops_prueba/terraform/main.tf
+++ devops_prueba/terraform/main.tf
@@ -1,4 +1,3 @@
-
 provider "google" {
   project = var.project_id
   region  = var.location
@@ -13,27 +12,4 @@ resource "google_container_cluster" "primary" {
   node_config {
     machine_type = var.machine_type
   }
-}
-
-output "kubernetes_cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "kubernetes_cluster_location" {
-  value = google_container_cluster.primary.location
-}
-
-output "kubernetes_cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
-  description = "La URL del endpoint del clúster de Kubernetes"
-}
-
-output "kubernetes_cluster_ca_certificate" {
-  value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-  description = "El certificado CA del clúster de Kubernetes"
-}
-
-output "kubernetes_cluster_node_pool_names" {
-  value = [for np in google_container_cluster.primary.node_pool : np.name]
-  description = "Los nombres de los pools de nodos del clúster"
 }
