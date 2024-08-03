c8a7e31cf87dc202d6d2a2232b8f51fcf6709ec7
ecastro5 <ecastro@skgtecnologia.com>
2024-08-02 20:43:17 -0500

    up

---
+++ devops_prueba/app/test.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, jsonify
+import pytest
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/')
+def home():
+    greeting = os.getenv('GREETING', '¡Hola, devco!')
+    return jsonify({"message": greeting})
+
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "¡Hola, devco!"}
